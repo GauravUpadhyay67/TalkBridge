3d263696fc825bccfd9f6b3797a771a142f45e91
Gaurav Upadhyay <upadhyaykumar2003@gmail.com>
2025-07-30 01:42:09 +0530

    Deployment of TalkBridge.

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from 'path';
+
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import connectDB from "./configs/mongodb.js";
@@ -10,6 +12,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(cors({
   origin: 'http://localhost:5173',
@@ -21,6 +25,14 @@ app.use(cookieParser())
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/chat', chatRouter);
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'))
+  })
+}
 
 connectDB()
   .then(() => {
