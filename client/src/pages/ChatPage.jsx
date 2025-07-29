87d79715ce1fa7425480c8255aa34855da8ec43a
Gaurav Upadhyay <upadhyaykumar2003@gmail.com>
2025-07-30 01:05:27 +0530

    Complete call page.

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,104 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import useAuthUser from '../hooks/useAuthUser';
-// import { useQuery } from '@tanstack/react-query';
-// import { getStreamToken } from '../lib/api';
-// import { Channel, ChannelHeader, Chat, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
-// import { StreamChat } from 'stream-chat';
-// import toast from 'react-hot-toast';
-// import ChatLoader from '../components/ChatLoader';
-
-// const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY
-
-// const ChatPage = () => {
-
-//   const { id: targetUserId } = useParams();
-
-//   const [chatClient, setChatClient] = useState(null);
-//   const [channel, setChannel] = useState(null);
-//   const [loading, setLoading] = useState(true);
-
-//   const { authUser } = useAuthUser();
-
-//   const { data: tokenData } = useQuery({
-//     queryKey: ['streamToken'],
-//     queryFn: getStreamToken,
-//     enabled: !!authUser // this will run only when authUser is available
-//   })
-
-
-//   useEffect(() => {
-//     const initChat = async () => {
-//       if(!tokenData?.token || !authUser) return;
-
-//       try {
-//         console.log('Initializing stream chat client...')
-//         const client = StreamChat.getInstance(STREAM_API_KEY);
-
-//         await client.connectUser({
-//           id: authUser._id,
-//           name: authUser.fullName,
-//           image: authUser.profilePic,
-//         }, tokenData.token)
-
-//         const channelId = [authUser._id, targetUserId].sort().join('-');
-
-//         const currChannel = client.channel('messaging', channelId, {
-//           members: [authUser._id, targetUserId],
-//         });
-
-//         await currChannel.watch();
-
-//         setChatClient(client);
-//         setChannel(currChannel);
-
-//       } catch (error) {
-//         console.error('Error initiating chat:', error);
-//         toast.error('Could not connect to chat. Please try again');
-//       }
-//       finally{
-//         setLoading(false);
-//       }
-//     }
-//   }, [tokenData, authUser, targetUserId]);
-
-//   if(loading || !chatClient || !channel) return <ChatLoader/>;
-
-//   return (
-//     <div className='h-[93vh]'>
-//       <Chat client={chatClient}>
-//         <Channel channel={channel}>
-//           <div className='w-full relative'>
-//             <Window>
-//               <ChannelHeader/>
-//               <MessageList/>
-//               <MessageInput focus/>
-//             </Window>
-//           </div>
-//         </Channel>
-//       </Chat>
-//     </div>
-//   )
-// }
-
-// export default ChatPage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useAuthUser from '../hooks/useAuthUser';
@@ -110,7 +9,7 @@ import toast from 'react-hot-toast';
 import ChatLoader from '../components/ChatLoader';
 import CallButton from '../components/CallButton';
 
-const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
 
 const ChatPage = () => {
 
